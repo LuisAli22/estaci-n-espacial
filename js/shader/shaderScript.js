9d6de5c703eeed490e1765632bf33ca6ba2fa992
LuisAli22 <luisali@fi.uba.ar>
2016-05-02 13:50:42 -0300

    Mejoro la escritura del objeto setUpExcecutableProgram poniendo los metodos en el padre, con prototype

--- js/shader/shaderScript.js
+++ js/shader/shaderScript.js
@@ -2,39 +2,7 @@ function shaderScript(gl,id){
   console.log("Constructor de shaderScript");
   this.script=document.getElementById(id);
   this.gl=gl;
-  this.isNotXFragmentAndNotXVertex=function(){
-    return (!this.isTypeXFragment()&&!this.isTypeXVertex());
-  }
-  this.isTypeXFragment=function(){
-    return (this.script.type == XSHADERXFRAGMENT);
-  }
-  this.isTypeXVertex=function(){
-    return (this.script.type == XSHADERXVERTEX);
-  }
-  this.isNodeTypeText=function(node){
-    return (node.nodeType == 3);
-  }
-  this.calculateTextContent=function (){
-    console.log("calculateTextContent busca el texto del script del shader, buscando entre sus hijos al nodo que tiene el texto");
-    var str = "";
-    var k = this.script.firstChild;
-    while (k) {
-        if (this.isNodeTypeText(k)) {
-          console.log("El nodo es un nodo de texto");
-            str += k.textContent;
-            console.log("Str: "+str);
-        }
-        k = k.nextSibling;
-    }
-    return str;
-  }
-  this.getCompiledShader=function(){
-    this.gl.shaderSource(this.shader, this.textContent);
-    this.gl.compileShader(this.shader);
-    if (!this.gl.getShaderParameter(this.shader, this.gl.COMPILE_STATUS))
-        throw new Error(this.gl.getShaderInfoLog(this.shader));
-    return this.shader;
-  }
+
   console.log("Si no puede obtener un script con el id "+id+" a continuacion va a tirar un Error");
   if (!this.script) throw new Error(SHADERERRORMESSAGE+": "+id);
   console.log("Si el script no es de tipo"+ XSHADERXFRAGMENT+" ni de tipo"+XSHADERXVERTEX+" va a tirar un error");
@@ -44,4 +12,37 @@ function shaderScript(gl,id){
               (this.gl.createShader(this.gl.FRAGMENT_SHADER)):
               (this.gl.createShader(this.gl.VERTEX_SHADER));
   console.log("Sale del constructor de shaderScript");
+}
+shaderScript.prototype.isNotXFragmentAndNotXVertex=function(){
+  return (!this.isTypeXFragment()&&!this.isTypeXVertex());
+}
+shaderScript.prototype.isTypeXFragment=function(){
+  return (this.script.type == XSHADERXFRAGMENT);
+}
+shaderScript.prototype.isTypeXVertex=function(){
+  return (this.script.type == XSHADERXVERTEX);
+}
+shaderScript.prototype.isNodeTypeText=function(node){
+  return (node.nodeType == 3);
+}
+shaderScript.prototype.calculateTextContent=function (){
+  console.log("calculateTextContent busca el texto del script del shader, buscando entre sus hijos al nodo que tiene el texto");
+  var str = "";
+  var k = this.script.firstChild;
+  while (k) {
+      if (this.isNodeTypeText(k)) {
+        console.log("El nodo es un nodo de texto");
+          str += k.textContent;
+          console.log("Str: "+str);
+      }
+      k = k.nextSibling;
+  }
+  return str;
+}
+shaderScript.prototype.getCompiledShader=function(){
+  this.gl.shaderSource(this.shader, this.textContent);
+  this.gl.compileShader(this.shader);
+  if (!this.gl.getShaderParameter(this.shader, this.gl.COMPILE_STATUS))
+      throw new Error(this.gl.getShaderInfoLog(this.shader));
+  return this.shader;
 }
