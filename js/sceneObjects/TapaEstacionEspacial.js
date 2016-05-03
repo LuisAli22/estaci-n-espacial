693d7659c72bb7b900559e9ee501ca9463bee598
mafvidal <mafvidal@gmail.com>
2016-05-03 12:32:06 -0300

    Mejoro iluminacion de las tapas

--- js/sceneObjects/TapaEstacionEspacial.js
+++ js/sceneObjects/TapaEstacionEspacial.js
@@ -16,6 +16,21 @@ function TapaEstacionEspacial(_bufferExterior,_bufferInterior){
         this.texture_coord_buffer = [];
         this.normal_buffer = [];
 
+        
+        
+        this.position_buffer = [];
+
+        this.position_buffer = this.position_buffer.concat(this.bufferExterior);
+        this.position_buffer = this.position_buffer.concat(this.bufferInterior);
+
+        //Calculo el vector normal a las tapas
+        var pos1 = vec3.fromValues(this.bufferExterior[0],this.bufferExterior[1],this.bufferExterior[2]);
+        var pos2 = vec3.fromValues(this.bufferInterior[0],this.bufferInterior[1],this.bufferInterior[2]);
+
+        var vectorNormal = vec3.create();
+        vec3.cross(vectorNormal,pos1,pos2);
+        vec3.normalize(vectorNormal,vectorNormal);
+
         //Cargo las coordenadas de textura
         for (var i = 0.0; i < this.rows; i++){
             for (var j = 0.0; j < this.cols; j++){
@@ -25,22 +40,17 @@ function TapaEstacionEspacial(_bufferExterior,_bufferInterior){
 
                 this.texture_coord_buffer.push(u);
                 this.texture_coord_buffer.push(v);
-                //Defino material 1--> color dorado
+                //Defino material --> color dorado
                 this.texture_coord_buffer.push(DORADO);
                 this.texture_coord_buffer.push(0);
 
-                this.normal_buffer.push(0.0);
-                this.normal_buffer.push(-1.0);
-                this.normal_buffer.push(0.0);
+                this.normal_buffer.push(-1.0*vectorNormal[0]);
+                this.normal_buffer.push(-1.0*vectorNormal[1]);
+                this.normal_buffer.push(-1.0*vectorNormal[2]);
 
             };
 
         };
-        
-        this.position_buffer = [];
-
-        this.position_buffer = this.position_buffer.concat(this.bufferExterior);
-        this.position_buffer = this.position_buffer.concat(this.bufferInterior);
 
         // Buffer de indices de los triangulos
         this.createIndexBuffer();
