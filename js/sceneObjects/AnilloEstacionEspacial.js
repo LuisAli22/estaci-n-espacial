79ac53d0acb8f77c8d243b9d070811dcef0e82ef
mafvidal <mafvidal@gmail.com>
2016-05-05 18:56:31 -0300

    Junto exteriorEstacionEspacial e interiorEstacionEspacial en anilloEstacionEspacial, eliminado metodos repetidos

--- js/sceneObjects/AnilloEstacionEspacial.js
+++ js/sceneObjects/AnilloEstacionEspacial.js
@@ -1,14 +1,18 @@
-ExteriorEstacionEspacial.prototype=new ComponenteEstacionEspacial;
-ExteriorEstacionEspacial.prototype.constructor=ExteriorEstacionEspacial;
-function ExteriorEstacionEspacial(){
+AnilloEstacionEspacial.prototype=new ComponenteEstacionEspacial;
+AnilloEstacionEspacial.prototype.constructor=AnilloEstacionEspacial;
+function AnilloEstacionEspacial(material){
 
     const PUNTOSCURVASUPERIOR = 30;
     const PUNTOSCURVAINFERIOR = 30;
     const PUNTOSCURVALATERAL = 10;
     const PUNTOSUNIONDECURVAS = 10;
+    
+    this.material = material;
+    this.bufferInicialCoordenadas = [];
+    this.bufferInicialNormales = [];
 
-ComponenteEstacionEspacial.call(this,FILASESTACIONESPACIAL,COLUMNASESTACIONESPACIAL);
-    this.cargarPerfil = function(bufferInicialCoordenadas,bufferInicialNormales){
+    ComponenteEstacionEspacial.call(this,FILASESTACIONESPACIAL,COLUMNASESTACIONESPACIAL);
+    this.cargarPerfilExterior = function(bufferInicialCoordenadas,bufferInicialNormales){
 
         //Curva superior del perfil
         var puntosDeControlXSuperior = vec4.fromValues(-2.5,3.75,0.75,-1.0);
@@ -132,7 +136,104 @@ ComponenteEstacionEspacial.call(this,FILASESTACIONESPACIAL,COLUMNASESTACIONESPAC
 
     }
 
-    this.initBuffers = function(){
+    this.cargarPerfilInterior = function(bufferInicialCoordenadas,bufferInicialNormales){
+
+        //Curva superior del perfil
+        var puntosDeControlX = vec4.fromValues(-0.5,0.75,0.75,-0.5);
+        var puntosDeControlY = vec4.fromValues(0.0,-1.5,1.5,0.5);
+
+        for(var i = 0.0 ; i < (this.cols / 4) ; i++ ) {
+
+            var t = i / ((this.cols / 4) - 1);
+
+            var x = Math.pow(t,3)* puntosDeControlX[0] + Math.pow(t,2)* puntosDeControlX[1] + t* puntosDeControlX[2] + puntosDeControlX[3];
+            var y = Math.pow(t,3)* puntosDeControlY[0] + Math.pow(t,2)* puntosDeControlY[1] + t* puntosDeControlY[2] + puntosDeControlY[3];
+            var z = 0.0;
+
+            bufferInicialCoordenadas.push(x);
+            bufferInicialCoordenadas.push(y);
+            bufferInicialCoordenadas.push(z);
+
+            var nx = 6.0*t* puntosDeControlX[0] + 2.0* puntosDeControlX[1];
+            var ny = 3.0*t* puntosDeControlY[0] + 2.0* puntosDeControlY[1];
+            var nz = 0.0;
+
+            var vectorNormal = vec3.fromValues(nx,ny,nz);
+            vec3.normalize(vectorNormal,vectorNormal);
+            bufferInicialNormales.push(vectorNormal[0]);
+            bufferInicialNormales.push(vectorNormal[1]);
+            bufferInicialNormales.push(vectorNormal[2]);
+
+        };
+        //Cargo lateral derecho
+        for(var i = 0.0 ; i < (this.cols / 4) ; i++ ) {
+
+            var t = i / ((this.cols / 4) - 1);
+
+            var x = 0.5;
+            var y = -1.0*t+0.5;
+            var z = 0.0;
+
+            bufferInicialCoordenadas.push(x);
+            bufferInicialCoordenadas.push(y);
+            bufferInicialCoordenadas.push(z);
+
+            var nx = -1.0;
+            var ny = 0.0;
+            var nz = 0.0;
+
+            bufferInicialNormales.push(nx);
+            bufferInicialNormales.push(ny);
+            bufferInicialNormales.push(nz);
+
+        };
+        //Cargo base
+        for(var i = 0.0 ; i < (this.cols / 4) ; i++ ) {
+
+            var t = i / ((this.cols / 4) - 1);
+
+            var x = -1.0*t+0.5;
+            var y = -0.5;
+            var z = 0.0;
+
+            bufferInicialCoordenadas.push(x);
+            bufferInicialCoordenadas.push(y);
+            bufferInicialCoordenadas.push(z);
+
+            var nx = 0.0;
+            var ny = 1.0;
+            var nz = 0.0;
+
+            bufferInicialNormales.push(nx);
+            bufferInicialNormales.push(ny);
+            bufferInicialNormales.push(nz);
+
+        };
+        //Cargo lateral izquierdo
+        for(var i = 0.0 ; i < (this.cols / 4) ; i++ ) {
+
+            var t = i / ((this.cols / 4) - 1);
+
+            var x = -0.5;
+            var y = 1.0*t-0.5;
+            var z = 0.0;
+
+            bufferInicialCoordenadas.push(x);
+            bufferInicialCoordenadas.push(y);
+            bufferInicialCoordenadas.push(z);
+
+            var nx = 1.0;
+            var ny = 0.0;
+            var nz = 0.0;
+
+            bufferInicialNormales.push(nx);
+            bufferInicialNormales.push(ny);
+            bufferInicialNormales.push(nz);
+
+        };
+    }
+
+    this.initBuffers = function(bufferInicialCoordenadas,bufferInicialNormales){
 
         this.texture_coord_buffer = [];
         this.position_buffer = [];
@@ -140,12 +241,6 @@ ComponenteEstacionEspacial.call(this,FILASESTACIONESPACIAL,COLUMNASESTACIONESPAC
 
         this.bufferInicial = [];
         this.bufferFinal = [];
-
-        var bufferInicialCoordenadas = [];
-        var bufferInicialNormales = [];
-
-        //Cargo el buffer inicial de coordenadas y normales del perfil
-        this.cargarPerfil(bufferInicialCoordenadas,bufferInicialNormales);
 
         //Cargo las coordenadas de textura
         for (var i = 0.0; i < this.rows; i++){
@@ -157,7 +252,7 @@ ComponenteEstacionEspacial.call(this,FILASESTACIONESPACIAL,COLUMNASESTACIONESPAC
                 this.texture_coord_buffer.push(u);
                 this.texture_coord_buffer.push(v);
                 //Defino material 1 --> dorado
-                this.texture_coord_buffer.push(DORADO);
+                this.texture_coord_buffer.push(this.material);
                 this.texture_coord_buffer.push(0);
             };
 
@@ -232,7 +327,16 @@ ComponenteEstacionEspacial.call(this,FILASESTACIONESPACIAL,COLUMNASESTACIONESPAC
         };
 
     }
-    this.initBuffers();
+    this.cargarExteriorEstacionEspacial = function(){
+
+        this.cargarPerfilExterior(this.bufferInicialCoordenadas,this.bufferInicialNormales);
+        this.initBuffers(this.bufferInicialCoordenadas,this.bufferInicialNormales);
+    }
+    this.cargarInteriorEstacionEspacial = function(){
+
+        this.cargarPerfilInterior(this.bufferInicialCoordenadas,this.bufferInicialNormales);
+        this.initBuffers(this.bufferInicialCoordenadas,this.bufferInicialNormales);
+    }
 }
 
 //inheritPrototype(ExteriorEstacionEspacial, DrawObject);
--- js/sceneObjects/Factory/FactoryEstacionEspacial.js
+++ js/sceneObjects/Factory/FactoryEstacionEspacial.js
@@ -1,8 +1,10 @@
 function FactoryEstacionEspacial(){}
 FactoryEstacionEspacial.prototype.crear=function(){
   var estacion=new composedSceneObject();
-  var interiorEstacionEspacial = new InteriorEstacionEspacial();
-  var exteriorEstacionEspacial = new ExteriorEstacionEspacial();
+  var interiorEstacionEspacial = new AnilloEstacionEspacial(VIOLETA);
+  var exteriorEstacionEspacial = new AnilloEstacionEspacial(DORADO);
+  exteriorEstacionEspacial.cargarExteriorEstacionEspacial();
+  interiorEstacionEspacial.cargarInteriorEstacionEspacial();
   var tapaInicial = new TapaEstacionEspacial(exteriorEstacionEspacial.bufferInicial,interiorEstacionEspacial.bufferInicial);
   var tapaFinal = new TapaEstacionEspacial(exteriorEstacionEspacial.bufferFinal,interiorEstacionEspacial.bufferFinal);
   estacion.push(interiorEstacionEspacial);
