6057150bd70a8638757d58b206700c03c8b871ea
LuisAli22 <luisali@fi.uba.ar>
2016-05-05 00:51:33 -0300

    Saco a fobos, diemos es el sol y marte es la tierra. Corro la estacion al centro de coordenadas. Corro la tierra hacia abajo en el eje Y

--- js/sceneObjects/Factory/FactoryEsferaLuna.js
+++
@@ -1,6 +0,0 @@
-function FactoryEsferaLuna(){}
-FactoryEsferaLuna.prototype.crear=function(){
-  var esfera = new TexturedSphere(64, 64,AMARILLO);
-  esfera.initTexture(IMGMOONPATH);
-  return esfera;
-}
--- js/sceneObjects/Factory/FactoryEsferaMarte.js
+++
@@ -1,6 +0,0 @@
-function FactoryEsferaMarte(){}
-FactoryEsferaMarte.prototype.crear=function(){
-  var esfera = new TexturedSphere(64, 64,CELESTE);
-  esfera.initTexture(IMGMARSPATH);
-  return esfera;
-}
