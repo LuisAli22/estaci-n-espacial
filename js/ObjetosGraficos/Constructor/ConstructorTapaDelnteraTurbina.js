5055e955ac354f6798e016a50248f51334e0eab5
mafvidal <mafvidal@gmail.com>
2016-06-24 01:59:04 -0300

    Agrego efecto de autoiluminacion a las turbinas de la nave

--- js/ObjetosGraficos/Constructor/ConstructorTapaDelnteraTurbina.js
+++ js/ObjetosGraficos/Constructor/ConstructorTapaDelnteraTurbina.js
@@ -14,7 +14,8 @@ ConstructorTapaDelnteraTurbina.prototype.definirMaterial=function(){
 	var material = new Material(this.componente.rows,this.componente.cols);
 
 	material.cargarTextura(RUTAIMAGENNAVE);
-	material.cargarCoordenadasDeTextura();	
+	material.cargarCoordenadasDeTextura();
+	material.agregarTexturaIluminacion(RUTAIMAGENNAVE);
 
 	Constructor.prototype.definirMaterial.call(this,material);
 
